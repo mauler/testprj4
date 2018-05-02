0ae48847acd499bdddd8964b7142df8c2945283f
Paulo R <proberto.macedo@gmail.com>
2018-05-02 09:21:12 -0300

    Added Issue service and tests

---
+++ issuer/service.py
@@ -0,0 +1,44 @@
+from decimal import Decimal
+import logging
+
+
+LOGGER = logging.getLogger(__name__)
+
+
+class IssuerService:
+    def __init__(self, db, currencies):
+        self._db = db
+        self._currencies = currencies
+
+    def load_money(self, card_id, amount, currency):
+        """Load money into an Account.
+
+        :param card_id: The card unique identification
+        :type card_id: str
+
+        :param amount: Amount to be loaded
+        :type amount: Decimal
+
+        :param currency: Currency code, 3 char long.
+        :type currency: str
+
+        :raises: AccountNotFound
+        """
+        if currency not in self._currencies:
+            raise ValueError('Currency "{}" not available.'.format(currency))
+
+        LOGGER.debug('Checking for account existence {}:{}',
+                     card_id,
+                     currency)
+
+        if not self._db.account_exists(card_id, currency):
+            LOGGER.warning('Account {}:{} not found.', card_id, currency)
+            self._db.create_account(card_id, currency)
+            LOGGER.info('Account {}:{} created.', card_id, currency)
+
+        self._db.load_money(card_id, Decimal(amount), currency)
+        LOGGER.info('{} {} loaded into account {}:{}.',
+                    amount,
+                    currency,
+                    card_id,
+                    currency)
---
+++ tests/test_issuer_service.py
@@ -0,0 +1,23 @@
+from unittest.mock import MagicMock
+from unittest import TestCase
+
+from issuer.service import IssuerService
+
+
+class IssuerServiceTests(TestCase):
+    CARD_ID = 'CARD123'
+    CURRENCY = 'BRL'
+
+    def setUp(self):
+        self.db_mock = MagicMock()
+        self.service = IssuerService(self.db_mock, [self.CURRENCY])
+
+    def test_account_exists(self):
+        # Ensure the account exists calls return False
+        self.db_mock.account_exists.return_value = False
+
+        # Call the load_money from the Service
+        self.service.load_money(self.CARD_ID, 100, self.CURRENCY)
+
+        # Create account should be called since the account doesn't exists
+        self.db_mock.create_account.assert_called()
